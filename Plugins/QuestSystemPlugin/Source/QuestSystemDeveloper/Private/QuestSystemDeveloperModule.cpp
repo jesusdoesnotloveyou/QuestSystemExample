bfb1eb52968abb41bc35a24afa6b5ae2f01eadd5
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2024-02-11 14:58:53 +0700

    added new developer module and changed build.cs file of runtime and editor modules

---
+++ Plugins/QuestSystemPlugin/Source/QuestSystemDeveloper/Private/QuestSystemDeveloperModule.cpp
@@ -0,0 +1,16 @@
+﻿#include "QuestSystemDeveloperModule.h"
+
+#define LOCTEXT_NAMESPACE "FQuestSystemDeveloperModule"
+
+void FQuestSystemDeveloperModule::StartupModule()
+{
+}
+
+void FQuestSystemDeveloperModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FQuestSystemDeveloperModule, QuestSystemDeveloper)
