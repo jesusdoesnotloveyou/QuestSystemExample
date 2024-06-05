e2d63a54552ea2fa39106d6e96ca655cddfc6867
Artyom Kozlovsky <artyomkozlovsky@gmail.com>
2024-06-05 19:30:09 +0700

    Added some engine code to runtime and editor modules for convenience.

--- Plugins/QuestSystemPlugin/Source/QuestSystemEditor/Public/QuestSystemEditorModule.h
+++ Plugins/QuestSystemPlugin/Source/QuestSystemEditor/Public/QuestSystemEditorModule.h
@@ -15,12 +15,33 @@ struct FQuestSystemGraphPinConnectionFactory;
 class FQuestSystemEditorModule : public IModuleInterface
 {
 public:
-	/** IModuleInterface implementation */
-    /* Called when the module is loaded */
-	virtual void StartupModule() override;
-    /* Called when the module is unloaded */
-	virtual void ShutdownModule() override;
+	/**
+     * Singleton-like access to this module's interface.  This is just for convenience!
+     * Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
+     *
+     * @return Returns singleton instance, loading the module on demand if needed
+     */
+    static inline FQuestSystemEditorModule& Get()
+    {
+    	return FModuleManager::LoadModuleChecked<FQuestSystemEditorModule>("QuestSystemEditor");
+    }
+    
+    /**
+     * Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
+     *
+     * @return True if the module is loaded and ready to use
+     */
+    static inline bool IsAvailable()
+    {
+    	return FModuleManager::Get().IsModuleLoaded("QuestSystemEditor");
+    }
 
+    /** IModuleInterface implementation */
+    /* Called when the module is loaded */
+    virtual void StartupModule() override;
+    /* Called when the module is unloaded */
+    virtual void ShutdownModule() override;
+    
 private:
     void RegisterAssetTypeAction(IAssetTools& AssetTools, TSharedRef<IAssetTypeActions> Action);
     
